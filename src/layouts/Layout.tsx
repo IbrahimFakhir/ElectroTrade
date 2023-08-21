6ecf42caa1efe9750e011d05a5533f8347f6a7a1
IbrahimFakhir <ibrahim.fakhir@outlook.de>
2023-08-21 22:27:48 +0200

    fixed navbar

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -4,7 +4,8 @@ import { Outlet } from "react-router-dom";
 const Layout = () => {
     return (
         <>
-            <aside className="absolute md:fixed bottom-0 w-screen md:w-56 h-16 md:h-screen flex md:flex-col justify-around items-center md:shadow-xl">
+            {/* previously absolute: md:fixed */}
+            <aside className="fixed bottom-0 w-screen md:w-56 h-16 md:h-screen flex md:flex-col justify-around items-center md:shadow-xl">
                 <Sidebar />
             </aside>
             <main className="fixed font-roboto overflow-y-auto h-mainMobile md:h-full md:min-h-full w-full md:w-mainDesktop md:ml-56 bg-customGrey">
