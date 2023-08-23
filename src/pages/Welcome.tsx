4870b67a56a65b3dc92d5e7e368838ff0ceb1eb8
IbrahimFakhir <ibrahim.fakhir@outlook.de>
2023-08-23 19:20:03 +0200

    added welcome page

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -3,8 +3,8 @@ import { Paper } from "@mui/material";
 const Welcome = () => {
     return(
         <div className="h-full flex justify-center items-center">
-            <Paper sx={{ padding: "2rem", width: "20rem" }} elevation={1}>
-                yo
+            <Paper sx={{ padding: "2rem", width: "20rem" }} elevation={1} className="flex justify-center">
+                Welcome
             </Paper>
         </div>
     )
