3c413c9a64674e6c135e85ba1800333f917edacc
IbrahimFakhir <ibrahim.fakhir@outlook.de>
2023-08-23 22:23:16 +0200

    added catch page

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import Market from "./pages/Market";
 import Account from "./pages/Account";
 import AuthLayout from "./layouts/AuthLayout/AuthLayout";
 import Authentication from "./pages/Authentication";
+import Missing from "./pages/Missing";
 import { pages, relativePagesPath } from "./utils/pages";
 
 function App() {
@@ -27,6 +28,8 @@ function App() {
 							<Route path={pages.get("market")?.path} element={<Market />} />
 							<Route path={pages.get("account")?.path} element={<Account />} />
 					</Route>
+
+					<Route path="*" element={<Missing />} />
 				</Routes>
 			</ThemeProvider>
 		</AuthProvider>
---
+++ src/pages/Missing.tsx
@@ -0,0 +1,9 @@
+const Missing = () => {
+    return(
+        <div className="bg-customGrey h-screen flex justify-center items-center">
+            <h1 className="font-semibold text-3xl">Missing Page!</h1>
+        </div>
+    )
+}
+
+export default Missing;
