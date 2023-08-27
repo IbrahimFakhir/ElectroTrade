b087afd2881ff80d6a69f6cb955f5e0c9e7e2ae1
IbrahimFakhir <ibrahim.fakhir@outlook.de>
2023-08-27 18:12:51 +0200

    style changes

--- src/pages/Market.tsx
+++ src/pages/Market.tsx
@@ -2,14 +2,22 @@ import { Stock } from "../features/stocks";
 
 const Market = () => {
 	return (
-		<div className="h-full md:mx-auto md:w-[95%] p-4">
+		<div className="h-full md:mx-auto md:w-[90%] 2xl:w-[80%] p-4">
 			<h1 className="text-3xl font-semibold mb-4 border-b border-black border-solid border-1">Market</h1>
-			<div className="md:mt-8 max-h-[90%] grid md:place-items-center xl:grid-cols-2 overflow-y-auto">
-				<div className="md:w-[34rem] xl:w-[30rem] 2xl:w-[34rem]">
+			<div className="md:mt-8 max-h-[90%] grid md:place-items-center xl:grid-cols-2 xl:gap-0 overflow-y-auto">
+				<div className="md:w-[34rem] xl:w-[28rem] 2xl:w-[30rem]">
+					<Stock />
+					<Stock />
+					<Stock />
+					<Stock />
 					<Stock />
 					<Stock />
 				</div>
-				<div className="md:w-[34rem] xl:w-[30rem] 2xl:w-[34rem]">
+				<div className="md:w-[34rem] xl:w-[28rem] 2xl:w-[30rem]">
+					<Stock />
+					<Stock />
+					<Stock />
+					<Stock />
 					<Stock />
 					<Stock />
 				</div>
