fbb32a7e4644d55bfab7483744b7546e9cd15fb9
IbrahimFakhir <ibrahim.fakhir@outlook.de>
2023-08-29 22:30:05 +0200

    updated api paths

--- src/lib/api-paths.ts
+++ src/lib/api-paths.ts
@@ -1,9 +1,9 @@
 const BASE_URL = "http://localhost:8080";
 
-const REGISTER_URL = "/api/v1/auth/register";
+const REGISTER_URL = "/auth/register";
 
-const LOGIN_URL = "/api/v1/auth/authenticate";
+const LOGIN_URL = "/auth/authenticate";
 
-const REFRESH_URL = "/api/v1/auth/refresh-token";
+const REFRESH_URL = "/auth/refresh-token";
 
 export { BASE_URL, REGISTER_URL, LOGIN_URL, REFRESH_URL };
