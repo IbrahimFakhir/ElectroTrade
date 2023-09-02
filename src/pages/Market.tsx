483ced1eea1fd63d26ff90b1eb2401ca5da618eb
IbrahimFakhir <ibrahim.fakhir@outlook.de>
2023-09-02 15:19:34 +0200

    dynamic stock list

--- src/pages/Market.tsx
+++ src/pages/Market.tsx
@@ -52,22 +52,18 @@ const Market = () => {
 		<div className="h-full md:mx-auto md:w-4/5 pt-6 md:pt-10 px-6 md:px-8">
 			<h1 className="text-3xl font-medium px-2 md:px-0 mb-4">Market</h1>
 			{/* alternative: md:justify-center 2xl:justify-between */}
+			<div className="max-h-[90%] overflow-y-auto flex md:grid flex-col md:grid-cols-[repeat(auto-fill,_36rem)] md:grid-rows-[repeat(auto-fill)] md:justify-evenly md:gap-x-2 gap-y-4 px-2">
 				{
-					stockData && 
-					<div className="max-h-[90%] overflow-y-auto flex md:grid flex-col md:grid-cols-[repeat(auto-fill,_36rem)] md:grid-rows-[repeat(auto-fill)] md:justify-evenly md:gap-x-2 gap-y-4 px-2">
+					stockData && stockData.stocks.map((stock, index) => 
 						<Stock 
-							name={stockData?.stocks[0].name} 
-							timestamps={stockData?.timestamps} 
-							priceHistory={stockData?.stocks[0].priceHistory} 
+							key={index}
+							name={stock.name}
+							timestamps={stockData?.timestamps}
+							priceHistory={stock.priceHistory}
 						/>
-						<Stock 
-							name={stockData?.stocks[0].name} 
-							timestamps={stockData?.timestamps} 
-							priceHistory={stockData?.stocks[0].priceHistory}						
-						/>
-					</div>
+					)
 				}
-			
+			</div>
 		</div>
 	)
 }
