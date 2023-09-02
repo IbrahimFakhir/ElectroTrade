68083a2237ee67fd1b6fbc458e54b36a0f598549
IbrahimFakhir <ibrahim.fakhir@outlook.de>
2023-09-02 10:16:20 +0200

    adjusted market grid

--- src/pages/Market.tsx
+++ src/pages/Market.tsx
@@ -2,9 +2,10 @@ import { Stock } from "../features/stocks";
 
 const Market = () => {
 	return (
-		<div className="h-full md:mx-auto md:w-4/5 pt-8 md:pt-10 px-8">
-			<h1 className="text-3xl font-medium mb-4">Market</h1>
-			<div className="max-h-[90%] overflow-y-auto flex md:grid flex-col md:grid-cols-[repeat(auto-fill,_36rem)] md:grid-rows-[repeat(auto-fill)] md:justify-center md:2xl:justify-between md:gap-x-2 gap-y-4">
+		<div className="h-full md:mx-auto md:w-4/5 pt-6 md:pt-10 px-6 md:px-8">
+			<h1 className="text-3xl font-medium px-2 md:px-0 mb-4">Market</h1>
+			{/* alternative: md:justify-center 2xl:justify-between */}
+			<div className="max-h-[90%] overflow-y-auto flex md:grid flex-col md:grid-cols-[repeat(auto-fill,_36rem)] md:grid-rows-[repeat(auto-fill)] md:justify-evenly md:gap-x-2 gap-y-4 px-2">
 				<Stock />
 				<Stock />
 				<Stock />
