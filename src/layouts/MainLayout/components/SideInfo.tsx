058ca3b684b1aa322efe851d467c3cede93da43b
IbrahimFakhir <ibrahim.fakhir@outlook.de>
2023-09-02 21:40:35 +0200

    added select none

--- src/layouts/MainLayout/components/SideInfo.tsx
+++ src/layouts/MainLayout/components/SideInfo.tsx
@@ -4,7 +4,7 @@ const SideInfo = () => {
     const { auth } = useAuth();
 
     return (
-        <div className="h-full flex flex-col justify-start items-center">
+        <div className="select-none h-full flex flex-col justify-start items-center">
             <div className="text-xl font-semibold flex justify-center items-center bg-customGrey w-20 h-20 rounded-full">
                 <span>{ auth?.name?.charAt(0) }{ auth?.name?.split(" ")[1][0] }</span>
             </div>
