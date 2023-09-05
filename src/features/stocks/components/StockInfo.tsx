b7c53ac9c700c05fc470fe48d420be3f39a04662
IbrahimFakhir <ibrahim.fakhir@outlook.de>
2023-09-05 19:00:57 +0200

    added isOwned

--- src/features/stocks/components/StockInfo.tsx
+++ src/features/stocks/components/StockInfo.tsx
@@ -18,8 +18,9 @@ const StockInfo = ({ stockId, stockName, stockPrice, quantityOwned, updateQuanti
         <>
             <div className="w-full md:w-2/5 px-2 md:px-0 md:flex md:flex-col md:justify-between mb-2 md:mb-0">
                 <div>
-                    <h1 className="font-semibold text-xl">{stockName}</h1>
+                    <h1 className="font-semibold text-xl mb-1">{stockName}</h1>
                     <p className="text-secondaryText">${stockPrice}</p>
+                    <span className="text-secondaryText">{quantityOwned} owned</span>
                 </div>
                 <div className="flex justify-between md:flex-col gap-3 mt-2 md:mb-8">
                     <Button 
